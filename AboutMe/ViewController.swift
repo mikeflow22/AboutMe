17b72370e25dd634b1267ab0f98a404fdd6ead69
Michael R.F <mrflowers22@uwalumni.com>
2019-04-23 13:32:30 -0400

    add introduce yourself button in interface that when pressed will display names and hobbies

--- AboutMe/ViewController.swift
+++ AboutMe/ViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var hobbieLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("My name is Michael. ")
-        print("Hello there.")
+//        print("My name is Michael. ")
+//        print("Hello there.")
+       
+    }
+    
+    @IBAction func introduceYourselfButtonPressed(_ sender: UIButton) {
+        nameLabel.text = "Michael Flowers"
+        hobbieLabel.text = "Basketball, weightlifting, traveling"
     }
 }
 
